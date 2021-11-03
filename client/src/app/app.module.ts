2fec5eea24bdb6b60db482aa65b8a2bdb426bcd7
Anderson-19 <andersonpantor@gmail.com>
2021-11-02 22:53:59 -0400

    Base Del Proyecto

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxsModule } from "@ngxs/store";
+import { TokenState } from "./store/token/token.state";
+import { StateAvatar } from "./store/avatar/avatar.state";
+import { StateEmail } from "./store/email/email.state";
+import { IonicStorageModule } from "@ionic/storage-angular";
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    NgxsModule.forRoot([TokenState, StateAvatar, StateEmail]),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
